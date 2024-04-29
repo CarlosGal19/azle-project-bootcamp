775237faf67eae003210851ace252842da6f2511
Carlos Galindo <utm22030587@utma.edu.mx>
2024-04-29 16:50:55 -0600

    feat: create post, get methods and type of array

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,25 @@ import cors from "cors";
 import express from 'express';
 
 export default Server(() => {
+
+    type Pet = {
+        id: number,
+        name: string,
+        owner: string,
+        email: string,
+        phone: string
+    };
+    
+    let pets: Pet[] = [
+        {
+            id: 1,
+            name: 'Mickey',
+            owner: 'Carlos',
+            email: 'carlos@gmail.com',
+            phone: '4405120993'
+        }
+    ];
+
     const app = express();
 
     app.use(cors());
@@ -17,10 +36,28 @@ export default Server(() => {
         }
     });
 
-    app.get('/whoami', (req, res) => {
-        res.statusCode = 200;
-        res.send(ic.caller());
+    app.get('/pets', (req, res) => {
+        res.send(pets);
     });
+
+    app.post('/pet', (req, res) => {
+        const { id, name, owner, email, phone } = req.body;
+        console.log(req.body);
+        if (!id || !name || !owner || !email || !phone) {
+            res.status(403);
+            return res.send({msg: 'All values are required'})
+        }
+        const newPet: Pet = {
+            id,
+            name,
+            owner,
+            email,
+            phone
+        };
+        pets = [...pets, newPet];
+        res.status(200);
+        return res.send('Pet Added')
+    })
 
     return app.listen();
 });
